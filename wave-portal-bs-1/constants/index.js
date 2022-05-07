436be6cd2b8bb0108b8dce11b812ace622a10ba2
zainamroti <sayedzainjeelani@gmail.com>
2022-05-07 18:32:42 +0500

    Add css + main wave method, connect wallet etc

---
+++ wave-portal-bs-1/constants/index.js
@@ -0,0 +1,67 @@
+export const WAVE_CONTRACT_ABI = [
+    {
+      "inputs": [],
+      "stateMutability": "nonpayable",
+      "type": "constructor"
+    },
+    {
+      "inputs": [],
+      "name": "getTotalWaves",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "user",
+          "type": "address"
+        }
+      ],
+      "name": "getUserWaveCount",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "waveeId",
+          "type": "uint256"
+        }
+      ],
+      "name": "getWavee",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "wave",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    }
+  ];
+export const WAVE_CONTRACT_ADDRESS = "0xfF84d88AaE91aB0E605859f9D253172a2e63815F";
+  
--- wave-portal-bs-1/pages/index.js
+++ wave-portal-bs-1/pages/index.js
@@ -1,13 +1,98 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import { useEffect, useState } from 'react';
+import styles from "../styles/Home.module.css";
+import { WAVE_CONTRACT_ABI, WAVE_CONTRACT_ADDRESS } from "../constants/index";
+import { ethers } from "ethers";
 
 export default function Home() {
 
-  const wave = () => {
+  /*
+  * Just a state variable we use to store our user's public wallet.
+  */
+  const [currentAccount, setCurrentAccount] = useState("");
 
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log("Make sure you have metamask!");
+        return;
+      } else {
+        console.log("We have the ethereum object", ethereum);
+      }
+
+      /*
+      * Check if we're authorized to access the user's wallet
+      */
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an authorized account:", account);
+        setCurrentAccount(account)
+      } else {
+        console.log("No authorized account found")
+      }
+    } catch (error) {
+      console.log(error);
+    }
   }
 
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+
+  /*
+  * This runs our function when the page loads.
+  */
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, [])
+
+  const wave = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const wavePortalContract = new ethers.Contract(WAVE_CONTRACT_ADDRESS, WAVE_CONTRACT_ABI, signer);
+
+         /*
+        * Execute the actual wave from your smart contract
+        */
+         const waveTxn = await wavePortalContract.wave();
+         console.log("Mining...", waveTxn.hash);
+ 
+         await waveTxn.wait();
+         console.log("Mined -- ", waveTxn.hash);
+ 
+
+        let count = await wavePortalContract.getTotalWaves();
+        console.log("Retrieved total wave count...", count.toNumber());
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
   return (
     <div>
       <Head>
@@ -30,12 +115,23 @@ export default function Home() {
           <button className={styles.waveButton} onClick={wave}>
             Wave at Me
           </button>
+
+          {/*
+        * If there is no currentAccount render this button
+        */}
+          {!currentAccount && (
+            <button className={styles.waveButton} onClick={connectWallet}>
+              Connect Wallet
+            </button>
+          )}
+
         </div>
+
+        <footer className={styles.footer}>
+          Made with &#10084; by SZeeS
+        </footer>
       </div>
 
-      <footer className={styles.footer}>
-        Made with &#10084; by SZeeS
-      </footer>
     </div>
   );
 }
