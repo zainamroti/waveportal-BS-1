32d2808043edd5e0a8a817c9e8a6049918d9eacc
zainamroti <sayedzainjeelani@gmail.com>
2022-05-08 09:57:02 +0500

    change wave contract to store messages ++

--- smart-contracts/contracts/WavePortal.sol
+++ smart-contracts/contracts/WavePortal.sol
@@ -9,16 +9,45 @@ contract WavePortal {
     mapping(address => uint) userWaveCount;
     address[] wavees;
 
+    event NewWave(address indexed from, uint256 timestamp, string message);
+
+    struct Wave {
+        address waver; // The address of the user who waved.
+        string message; // The message the user sent.
+        uint256 timestamp; // The timestamp when the user waved.
+    }
+
+    /*
+     * I declare a variable waves that lets me store an array of structs.
+     * This is what lets me hold all the waves anyone ever sends to me!
+     */
+    Wave[] waves;
+
     constructor() {
         console.log("Yo yo, I am a smart contract developer and I am smart ;)");
     }
 
-    function wave() public {
+    function wave(string memory _message) public {
         userWaveCount[msg.sender] = userWaveCount[msg.sender]++;
         console.log("%d totalWaves!", totalWaves);
         wavees.push(msg.sender);
         totalWaves += 1;
         console.log("%s has waved!", msg.sender);
+
+        /*
+         * This is where I actually store the wave data in the array.
+         */
+        waves.push(Wave(msg.sender, _message, block.timestamp));
+
+        /*
+         * I added some fanciness here, Google it and try to figure out what it is!
+         * Let me know what you learn in #general-chill-chat
+         */
+        emit NewWave(msg.sender, block.timestamp, _message);
+    }
+
+    function getAllWaves() public view returns (Wave[] memory) {
+        return waves;
     }
 
     function getTotalWaves() public view returns (uint256) {
